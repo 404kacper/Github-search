a8024bb6028d418faa585e4de3799351d56b6c64
Kacper <kacper.sionkowsk@gmail.com>
2019-07-17 00:14:29 +0200

    Initial commit

---
+++ build/precache-manifest.7ac3ba2865bd8d6bf78db7595b49ab8d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e4c63cc6ef9553e32a63bec3c66d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efe5585e846382f5b6b7",
+    "url": "/static/css/main.f65227a3.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "efe5585e846382f5b6b7",
+    "url": "/static/js/main.7d307ad4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
